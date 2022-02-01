5847f91c4b414eeedbc05c6ef386844437c2c5e6
Dhruv Kaushik <93424205+dhruvk1998@users.noreply.github.com>
2022-02-01 15:54:22 +0530

    Add files via upload

---
+++ Calculator 3.py
@@ -0,0 +1,16 @@
+a = input(" Number 1 :")
+if a == '':
+    print("Enter a valid number")
+
+b = input(" Number 2 :")
+if b == '':
+    print("Enter a valid number ")
+    print ("Check your values and try again later. \nGood luck" )
+    exit()
+
+# while True:
+#     if a=='' and b=='':
+        
+else:
+    print(" sum of the two nyumbers = ",  float(a)+float(b))   
+
