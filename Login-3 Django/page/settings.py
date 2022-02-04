87bca74c389cd57db8c710182fe8fa05bac390ed
Dhruv Kaushik <93424205+dhruvk1998@users.noreply.github.com>
2022-02-04 10:44:22 +0530

    Update settings.py

--- Login-3 Django/page/settings.py
+++ Login-3 Django/page/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&5)og088awol)p)ddiu=)ogd20-&uxb=yj$lca6puo_h5=m3bg'
+SECRET_KEY = #Fill your own secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
